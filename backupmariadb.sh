cc496fff8ba0cf2fda0c67b76d74b0fc4b2c4ffc
José Almeida <josemedeirosdealmeida@gmail.com>
2017-12-07 09:39:19 +0000

    Update backupmariadb.sh

--- backupmariadb.sh
+++ backupmariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# jose.almeida@gfi.pt
+# josemedeirosdealmeida@gmail.com jose.almeida@gfi.pt
 # Jose Almeida
 touch /var/log/backupmariadb.log
 chmod 777 /var/log/backupmariadb.log
@@ -12,3 +12,4 @@ ssh prdlsup038 "mysql -u root < /tmp/mysql-backup_prdlsup028_*.sql"
 ssh prdlsup038 "gzip /tmp/mysql-backup_prdlsup028_*.sql"
 ssh prdlsup038 "rm -f /tmp/mysql-backup_prdlsup028_*.sql"
 ssh prdlsup038 "find /tmp/*prdlsup028* -mtime +20 -exec rm -f {} \;"
+rm -f /tmp/mysql-backup_prdlsup028_*.sql
