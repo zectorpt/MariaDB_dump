2cf7e9d7add306c3e41795d04ac122254a806edb
José Almeida <josemedeirosdealmeida@gmail.com>
2017-12-11 10:48:49 +0000

    Update backupmariadb.sh

--- backupmariadb.sh
+++ backupmariadb.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-#
-# josemedeirosdealmeida@gmail.com jose.almeida@gfi.pt
-# Jose Almeida
 touch /var/log/backupmariadb.log
 chmod 777 /var/log/backupmariadb.log
 mysqldump -u root --all-databases > /tmp/mysql-backup_prdlsup028_$(date +\%Y\%m\%d_\%H\%M\%S).sql
@@ -11,5 +7,6 @@ scp /tmp/mysql-backup_prdlsup028_*.sql root@prdlsup038:/tmp/
 ssh prdlsup038 "mysql -u root < /tmp/mysql-backup_prdlsup028_*.sql"
 ssh prdlsup038 "gzip /tmp/mysql-backup_prdlsup028_*.sql"
 ssh prdlsup038 "rm -f /tmp/mysql-backup_prdlsup028_*.sql"
-ssh prdlsup038 "find /tmp/*prdlsup028* -mtime +20 -exec rm -f {} \;"
+ssh prdlsup038 "find /tmp/*prdlsup028* -type f -mtime +6 -exec rm -f {} \;"
+#Just in case
 rm -f /tmp/mysql-backup_prdlsup028_*.sql
